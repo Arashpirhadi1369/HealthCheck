962793dc496a35842b3cb9df8ee95c174cedf946
unknown <Arash.pir@gmail.com>
2022-08-07 11:50:17 +0430

    added healthcheck for hamkaran system

---
+++ app/Jobs/HealthCheck.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Hamkaran;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class HealthCheck implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        try {
+            Http::get("http://localhost:7001/oa");
+        } catch (\Throwable $th) {
+            exec('C:\bea\user_projects\domains\oadomain\bin\startWebLogic.cmd');
+
+            $hamkaran = new Hamkaran();
+
+            $hamkaran->save();
+        }
+    }
+}
